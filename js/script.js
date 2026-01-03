8fd6fefa9e3c9a80554d95a68fb4bfc8e5792a5d
chrll17 <119146753+chrll17@users.noreply.github.com>
2026-01-03 16:58:22 +0700

    handle pengkondisian

--- js/script.js
+++ js/script.js
@@ -8,7 +8,31 @@ function searchFilm(){
             's' : $('#search-input').val() // Kata kunci pencarian dari input
         },
         success: function (result){ // Callback jika request sukses
-            console.log(result) // testing hasil di console browser (debugging)
+            if (result.Response == "True") {
+                let films = result.Search
+                $.each(films, function(i,data){
+                    $('#film-list').append(`
+                        <div class="col">
+                            
+                            <div class="card" style="width: 15rem;">
+                                <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">`+ data.Title +`</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+                                    <a href="#" class="card-link">detail</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    `)
+                })
+            }else{
+                $('#film-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error + `</h1>
+                    </div>    
+                `)
+            }
         }
     })
 }
